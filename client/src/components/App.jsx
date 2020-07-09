6f4fd1422571f8004552b5b6f7f3d1bb8508089e
Henrique Parreiras <henrique5088@hotmail.com>
2020-07-09 13:53:35 -0300

    google OAuth

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import StreamList from "./streams/StreamList";
-import StreamCreate from "./streams/StreamCreate";
-import StreamDelete from "./streams/StreamDelete";
-import StreamEdit from "./streams/StreamEdit";
-import StreamShow from "./streams/StreamShow";
-import Header from "./streams/Header";
+import StreamList from './streams/StreamList';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import Header from './streams/Header';
 
 const App = () => {
 	return (
---
+++ client/src/components/GoogleAuth.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
+
+class GoogleAuth extends Component {
+	componentDidMount() {
+		window.gapi.load('client:auth2', () => {
+			window.gapi.client
+				.init({
+					clientId:
+						'62482545670-550h5egteot5mb9a95dsrrbc3s3c2tkp.apps.googleusercontent.com',
+					scope: 'email',
+				})
+				.then(() => {
+					this.auth = window.gapi.auth2.getAuthInstance();
+					this.onAuthChange(this.auth.isSignedIn.get());
+					this.auth.isSignedIn.listen(this.onAuthChange);
+				});
+		});
+	}
+
+	onAuthChange = (isSignedIn) => {
+		if (isSignedIn) {
+			this.props.signIn();
+		} else {
+			this.props.signOut();
+		}
+	};
+
+	onSignInClick = () => {
+		this.auth.signIn();
+	};
+
+	onSignOutClick = () => {
+		this.auth.signOut();
+	};
+
+	renderAuthButton() {
+		if (this.props.isSignedIn === null) {
+			return null;
+		} else if (this.props.isSignedIn) {
+			return (
+				<button
+					onClick={this.onSignOutClick}
+					className="ui red google button">
+					<i className="google icon" />
+					Sign Out
+				</button>
+			);
+		} else {
+			return (
+				<button
+					onClick={this.onSignInClick}
+					className="ui red google button">
+					<i className="google icon"></i>
+					Sign In With Google
+				</button>
+			);
+		}
+	}
+
+	render() {
+		return <div>{this.renderAuthButton()}</div>;
+	}
+}
+
+const mapStateToProps = (state) => ({
+	isSignedIn: state.auth.isSignedIn,
+});
+
+export default connect(mapStateToProps, { signIn, signOut })(GoogleAuth);
